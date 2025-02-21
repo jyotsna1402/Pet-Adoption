a207664d19eb2fb15e8cd02e9bb3b74e04a7b3ec
Jyostna Roy Chowdhury <jyostnaroychowdhury@gmail.com>
2025-02-21 12:34:39 +0530

    Add new files

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello() {
+    alert("Hello from JavaScript!");
+  }
+  
+  // Call the function when a button is clicked (assuming you have a button with id="myButton")
+  document.getElementById("myButton").addEventListener("click", sayHello);
+  
